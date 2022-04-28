19a65358e5ab62eb4d621201155b4f01e04ebdce
zhangzeyu007 <zhangzeyu1234@gmail.com>
2022-04-28 11:09:10 +0800

    feat: 生命周期

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,21 +3,43 @@
  * @Author: 张泽雨
  * @Date: 2022-04-28 10:05:26
  * @LastEditors: 张泽雨
- * @LastEditTime: 2022-04-28 10:06:34
+ * @LastEditTime: 2022-04-28 11:08:23
  * @FilePath: \vue3-study\src\components\HelloWorld.vue
 -->
 
 <template>
-
-<input type="text" name="" placeholder="你好">
- 
+    <input type="text" name="" placeholder="你好" />
+    <div>{{ num }}</div>
+    <a-button @click="num++">增加</a-button>
 </template>
 
-
 <script setup lang="ts">
+/// 父子组件的生命周期
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, ref } from 'vue';
 
+const num = ref(0);
 
+onBeforeMount(() => {
+    console.log('子组件---挂载之前---onBeforeMount');
+});
 
+onMounted(() => {
+    console.log('子组件---挂载----onMounted');
+});
 
+onBeforeUpdate(() => {
+    console.log('子组件---更新之前----onBeforeUpdate');
+});
 
+onUpdated(() => {
+	console.log('子组件---完成更新--onUpdate');
+});
+
+onBeforeUnmount(() => {
+    console.log('子组件---卸载之前-----onBeforeUnmount');
+});
+
+onUnmounted(() => {
+    console.log('子组件---卸载完成----onUnmounted');
+});
 </script>
--- src/view/about/component.vue
+++ src/view/about/component.vue
@@ -3,20 +3,50 @@
  * @Author: 张泽雨
  * @Date: 2022-04-28 09:59:12
  * @LastEditors: 张泽雨
- * @LastEditTime: 2022-04-28 10:08:38
+ * @LastEditTime: 2022-04-28 11:04:21
  * @FilePath: \vue3-study\src\view\about\component.vue
 -->
+
 <template>
 
-<HelloWorld></HelloWorld>
+<HelloWorld v-if="flag"></HelloWorld>
+<a-button @click="flag = !flag">改变组件状态</a-button>
 
 </template>
 
 <script setup lang="ts">
 
 import HelloWorld from "@/components/HelloWorld.vue"
+import { ref , onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted,} from 'vue'
 
+const flag = ref(true)
 
+const change = () =>{
+ 
+}
 
+onBeforeMount(()=>{
+	console.log('父组件----挂载之前---onBeforeMount');
+})
+
+onMounted(()=>{
+	console.log('父组件----挂载----onMounted');
+})
+
+onBeforeUpdate(()=>{
+	console.log('父组件----更新之前----onBeforeUpdate');
+})
+
+onUpdated(()=>{
+	console.log('父组件----完成更新--onUpdate');
+})
+
+onBeforeUnmount(()=>{
+	console.log('父组件----卸载之前-----onBeforeUnmount');
+})
+
+onUnmounted(()=>{
+	console.log('父组件----卸载完成----onUnmounted');
+})
 
 </script>
